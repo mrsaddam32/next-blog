b552c864c395c04260bc69fb8ee7659009eb5d07
mrsaddam32 <neolinedev@gmail.com>
2021-09-08 18:55:54 +0700

    Changing the fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import "../styles/global.css";
+import "../styles/fonts.css";
 import { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,5 +1,11 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default (_: NextApiRequest, res: NextApiResponse) => {
-  res.status(200).json({ text: "Hello neoline" });
+  res.status(200).json({
+    nama: "Muhammad Saddam Pradana",
+    nim: 10190130,
+    jurusan: "Rekayasa Perangkat Lunak",
+    hobi: ["Coding", "Gaming"],
+    lulus: false,
+  });
 };
